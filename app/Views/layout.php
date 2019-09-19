6c8f27c276a19aa2ed09d4056e060f5b1db9b0d6
denis303 <dev@denis303.com>
2019-09-19 15:39:49 +0300

    n/a

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -10,13 +10,13 @@ use denis303\bootstrap4\Alert;
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="/libs/bootstrap4/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?= base_url();?>/libs/bootstrap4/dist/css/bootstrap.min.css">
 <title><?= empty($this->data['title']) ? 'Default app title' : esc($this->data['title']);?></title>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="/libs/jquery/dist/jquery.min.js"></script>
-<script src="/libs/popper.js/dist/umd/popper.min.js"></script>
-<script src="/libs/bootstrap4/dist/js/bootstrap.min.js"></script>
+<script src="<?= base_url();?>/libs/jquery/dist/jquery.min.js"></script>
+<script src="<?= base_url();?>/libs/popper.js/dist/umd/popper.min.js"></script>
+<script src="<?= base_url();?>/libs/bootstrap4/dist/js/bootstrap.min.js"></script>
 <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 </head>
 <body>
