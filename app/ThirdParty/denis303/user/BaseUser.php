dbca0ec19a308968c32ea6f0e3b9971afd5827e5
denis303 <dev@denis303.com>
2019-09-20 09:48:45 +0300

    n/a

--- app/ThirdParty/denis303/user/BaseUser.php
+++ app/ThirdParty/denis303/user/BaseUser.php
@@ -2,8 +2,6 @@
 
 namespace denis303\user;
 
-require_once APPPATH . '\Components\Entity.php';
-
 abstract class BaseUser extends \App\Components\Entity
 {
 }
--- app/ThirdParty/denis303/user/BaseUserModel.php
+++ app/ThirdParty/denis303/user/BaseUserModel.php
@@ -2,8 +2,6 @@
 
 namespace denis303\user;
 
-require_once APPPATH . '\Components\Model.php';
-
 abstract class BaseUserModel extends \App\Components\Model
 {
 }
